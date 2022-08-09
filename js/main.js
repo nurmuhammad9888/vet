8e131bb70d6dcdbf5677a54eae6f1b145a390149
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-08-09 11:50:08 +0500

    Add new page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let elBtnOpen = document.querySelector(".site-header__btn-text");
+let elModal = document.querySelector(".modal");
+let elBtnClose = document.querySelector(".modal__close-btn");
+let elBody = document.querySelector("body");
+elBtnOpen.addEventListener("click", function(vet){
+    vet.preventDefault
+    elModal.classList.add("modal-active")
+    elBody.classList.add("body-active")
+})
+elBtnClose.addEventListener("click", function(){
+    elModal.classList.remove("modal-active")
+    elBody.classList.remove("body-active")
+})
