3b5c32f2d26f4ba46a71ef6365c01855e0713cca
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-08-10 11:54:14 +0500

    Add new page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+
 let elBtnOpen = document.querySelector(".site-header__btn-text");
 let elModal = document.querySelector(".modal");
 let elBtnClose = document.querySelector(".modal__close-btn");
 let elBody = document.querySelector("body");
+
+
 elBtnOpen.addEventListener("click", function(vet){
     vet.preventDefault
     elModal.classList.add("modal-active")
